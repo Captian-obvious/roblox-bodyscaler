73024ff8649b0feb67073e40c922c13da0692614
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-22 14:57:40 -0500

    Update scale.lua

--- scale.lua
+++ scale.lua
@@ -1,6 +1,19 @@
-local values {
-   headscale=Humanoid.HeadScale.Value;
-    bodyWidth = Humanoid.BodyWidthScale.Value;
-    bodyHeight = Humanoid.BodyHeightScale; 
-    
-}
+local bodyScaleModule = {};
+
+function bodyScaleModule.ScaleR15(p, scale)
+    local plr = game:GetService("Players"):WaitForChild(p)
+    local Character = plr.Character
+    local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+    local values {
+        headScale=Humanoid.HeadScale;
+        bodyWidth = Humanoid.BodyWidthScale;
+        bodyHeight = Humanoid.BodyHeightScale; 
+        bodyDepth = Humanoid.BodyDepthScale;
+    };
+    values.bodyWidth.Value = scale;
+    values.bodyHeight.Value = scale;
+    values.bodyDepth.Value = scale;
+    values.headScale.Value = scale;
+end
+
+return bodyScaleModule
