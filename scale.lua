127761f56b25657a0a0af955f2386c44f41f939b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-22 18:40:53 -0500

    Update scale.lua

--- scale.lua
+++ scale.lua
@@ -1,7 +1,9 @@
 local bodyScaleModule = {};
 
+local Players = game:GetService("Players");
+
 function ScaleR15(p, scale)
-    local plr = game:GetService("Players"):WaitForChild(p)
+    local plr = Players:WaitForChild(p)
     local Character = plr.Character
     local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
     local values = {
@@ -16,7 +18,8 @@ function ScaleR15(p, scale)
     values.headScale.Value = scale;
 end
 
-function bodyScaleModule.ScaleCharacter(plr, scale, isR15)
+function bodyScaleModule.ScaleCharacter(plr, scale)
+    local isR15 = Players:WaitForChild(plr).Character:WaitForChild("Humanoid").RigType == Enum.HumanoidRigType.R15
     if isR15 == true then
         ScaleR15(plr, scale)
     end
