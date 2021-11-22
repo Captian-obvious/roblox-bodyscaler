7e971ea6df67d28c861866c1750ca81eccd863d0
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-22 18:36:27 -0500

    Create e.lua

---
+++ e.lua
@@ -0,0 +1,16 @@
+function ScaleR15(p, scale)
+    local plr = game:GetService("Players"):WaitForChild(p)
+    local Character = plr.Character
+    local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+    local values {
+        headScale=Humanoid.HeadScale;
+        bodyWidth = Humanoid.BodyWidthScale;
+        bodyHeight = Humanoid.BodyHeightScale; 
+        bodyDepth = Humanoid.BodyDepthScale;
+    };
+    values.bodyWidth.Value = scale;
+    values.bodyHeight.Value = scale;
+    values.bodyDepth.Value = scale;
+    values.headScale.Value = scale;
+end
+ScaleR15("Superduperbloxer2",0.5)
