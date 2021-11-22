de027385c77070e43cab623864be5b52790491e8
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-22 18:43:18 -0500

    Update scale.lua

--- scale.lua
+++ scale.lua
@@ -2,26 +2,31 @@ local bodyScaleModule = {};
 
 local Players = game:GetService("Players");
 
-function ScaleR15(p, scale)
+function Scale(p, scale)
     local plr = Players:WaitForChild(p)
     local Character = plr.Character
     local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
-    local values = {
-        headScale=Humanoid.HeadScale;
-        bodyWidth = Humanoid.BodyWidthScale;
-        bodyHeight = Humanoid.BodyHeightScale; 
-        bodyDepth = Humanoid.BodyDepthScale;
-    };
-    values.bodyWidth.Value = scale;
-    values.bodyHeight.Value = scale;
-    values.bodyDepth.Value = scale;
-    values.headScale.Value = scale;
+    if Humanoid.RigType == Enum.HumanoidRigType.R15 then
+        local values = {
+            headScale=Humanoid.HeadScale;
+            bodyWidth = Humanoid.BodyWidthScale;
+            bodyHeight = Humanoid.BodyHeightScale; 
+            bodyDepth = Humanoid.BodyDepthScale;
+        };
+        values.bodyWidth.Value = scale;
+        values.bodyHeight.Value = scale;
+        values.bodyDepth.Value = scale;
+        values.headScale.Value = scale;
+    else
+        error("Player Must Be R15 for this to work.")
+        return 
+    end
 end
 
 function bodyScaleModule.ScaleCharacter(plr, scale)
     local isR15 = Players:WaitForChild(plr).Character:WaitForChild("Humanoid").RigType == Enum.HumanoidRigType.R15
     if isR15 == true then
-        ScaleR15(plr, scale)
+        Scale(plr, scale)
     end
 end
 
